142243180fed0523e9cceb73090aeb4e70a22d1d
Larry Karnowski <larry.karnowski@gmail.com>
2010-05-21 10:59:21 -0400

    Added sample "message received" event documentation for future reference.

---
+++ doc/sample_event.js
@@ -0,0 +1,11 @@
+var event = {
+  content: "saying stuff and doing things",
+  id: "6f5765c04715012d0c1012313d01d943",
+  time: 1274453133,
+  type: "message",
+  user: {
+    email: "larry@hickorywind.org",
+    id: 3073,
+    name: "larry"
+  }
+};
